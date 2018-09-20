0592a5b71b6f88bdecf33e8913f10ac5cd60ca30
HAMISH LYALL <101614208@student.swin.edu.au>
2018-09-20 11:10:00 +1000

    First Build

---
+++ 2ndPracChallenge/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_2ndPracChallenge.MvcApplication" Language="C#" %>
---
+++ 2ndPracChallenge/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_2ndPracChallenge.Startup))]
+namespace _2ndPracChallenge
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
